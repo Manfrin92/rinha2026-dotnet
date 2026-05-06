472b556a268aa949f4379bbfc5e2e1f4203405b0
Manfrin <manfredd_10@hotmail.com>
2026-05-06 17:04:28 -0300

    feat: truncating vector from 14 indexes to 5

--- RinhaApi/Program.cs
+++ RinhaApi/Program.cs
@@ -7,12 +7,17 @@ using RinhaApi.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-builder.Services.AddScoped<IFraudDetectionService, FraudDetectionService>();
 builder.Services.AddScoped<IVector, Vector>();
 builder.Services.AddScoped<IMatrix, Matrix>();
-var app = builder.Build();
 
 var process = Process.GetCurrentProcess();
+var vectorSize = 5;
+int capacity = 3_000_000;
+
+var vectors = new byte[capacity * vectorSize];
+var labels = new byte[capacity];
+
+int index = 0;
 int legitCount = 0;
 int fraudCount = 0;
 Console.WriteLine($"Managed memory: {GC.GetTotalMemory(false) / (1024 * 1024)} MB");
@@ -20,21 +25,60 @@ Console.WriteLine($"Working set: {process.WorkingSet64 / (1024 * 1024)} MB");
 
 // Using Streams to read the file one by one and not loading everything in memory
 using var gz = new GZipStream(File.OpenRead("./references.json.gz"), CompressionMode.Decompress);
-await foreach (var r in JsonSerializer.DeserializeAsyncEnumerable<Reference>(
-    gz, RefJsonContext.Default.Reference))
+await foreach (var r in JsonSerializer.DeserializeAsyncEnumerable(gz, RefJsonContext.Default.Reference))
 {
-    if (r is null) continue;
-
-    if (r.Label == "legit")
+    // --- label ---
+    if (r?.Label == "legit")
+    {
+        labels[index] = 0;
         legitCount++;
-    else if (r.Label == "fraud")
+    }
+    else
+    {
+        labels[index] = 1;
         fraudCount++;
+    }
+
+    // --- dimensionality reduction by truncation
+    int baseOffset = index * vectorSize;
+
+    for (int i = 0; i < vectorSize; i++)
+    {
+        var value = r?.Vector[i]; // double between 0–1
+
+        // clamp just in case
+        if (value < 0) value = 0;
+        if (value > 1) value = 1;
+
+        if (value != null)
+        {
+            vectors[baseOffset + i] = (byte)(value * 255);
+        }
+    }
+
+    index++;
 }
 
 Console.WriteLine($"Loaded {legitCount+fraudCount} references.");
 Console.WriteLine($"Managed memory: {GC.GetTotalMemory(false) / (1024 * 1024)} MB");
 Console.WriteLine($"Working set: {process.WorkingSet64 / (1024 * 1024)} MB");
 Console.WriteLine($"Legit: {legitCount}, Fraud: {fraudCount}");
+
+Console.WriteLine("labels size: {0}", labels.Length);
+Console.WriteLine("vectors size: {0}", vectors.Length);
+
+var fraudService = new FraudDetectionService(
+    legitCount,
+    fraudCount,
+    labels,
+    vectors,
+    vectorSize,
+    index
+);
+
+builder.Services.AddSingleton<IFraudDetectionService>(fraudService);
+
+var app = builder.Build();
 
 app.MapControllers(); 
 
--- RinhaApi/Services/FraudDetectionService.cs
+++ RinhaApi/Services/FraudDetectionService.cs
@@ -2,8 +2,27 @@ using RinhaApi.Controllers.Dtos;
 
 namespace RinhaApi.Services;
 
-public class FraudDetectionService : IFraudDetectionService
+public class FraudDetectionService(int legitCount,
+    int fraudCount,
+    byte[] labels,
+    byte[] vectors,
+    int vectorSize,
+    int count) : IFraudDetectionService
 {
+    private int _legitCount = legitCount;
+
+    private int _fraudCount = fraudCount;
+
+    // All references stored in one single array
+    private byte[] _labels = labels; // 0 = legit, 1 = fraud
+
+    // Super simplified version of the references
+    private byte[] _vectors = vectors;
+
+    private int _count = count;
+
+    private int _vectorSize = vectorSize;
+
     public FraudScoreResponse IsFraudulent(FraudScoreRequest request)
     {
         FraudScoreResponse response = new(Approved: true, Fraud_score: 0);
@@ -13,6 +32,10 @@ public class FraudDetectionService : IFraudDetectionService
 
     public bool IsReady()
     {
-        return true;
+        Console.WriteLine($"FraudDetectionService - Legit count: {_legitCount}, Fraud count: {_fraudCount}");
+        Console.WriteLine($"FraudDetectionService - Labels length: {_labels?.Length}, Vectors length: {_vectors?.Length}");
+        Console.WriteLine($"FraudDetectionService - Vector size: {_vectorSize}, Count: {_count}");
+
+        return _legitCount + _fraudCount > 0 && _labels != null && _labels.Length > 0 && _vectors != null && _vectors.Length > 0;
     }
 }
